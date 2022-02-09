8c8758abbc4b33f96ee5fbd1f90dee0020a8117b
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-09 17:44:22 +0200

    add footer

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<!-- Page level plugin JavaScript-->
+<script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('vendor/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('vendor/jquery.magnific-popup.min.js') }}"></script>
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('js/admin.js') }}"></script>
+<!-- Custom scripts for this page-->
+<script src="{{ asset('vendor/dropzone.min.js') }}"></script>
