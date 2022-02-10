9784fc1db54dc76c5d5f48a080b44e54e49250b5
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-10 04:55:06 +0200

    index admin page

--- app/Http/Controllers/TravelController.php
+++ app/Http/Controllers/TravelController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class TravelController extends Controller
 {
+    public function index() {
+        return view('admin.Pages.travel');
+    }
+
     public function upsert(TravelRequest $request) {
         $travel = Travel::upsertInstance($request);
     }
