12ec6656fa700ac86d04824664b9248ddde6e0e6
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-11 13:49:46 +0200

    city route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
@@ -30,5 +31,12 @@ Route::group(['prefix' => 'admin'],function(){
         Route::get('/{hotel}',[HotelController::class,'getHotel'])->name('hotel.get');
         Route::post('/upsert',[HotelController::class,'upsert'])->name('hotel.upsert');
         Route::post('/delete/{hotel}',[HotelController::class,'delete'])->name('hotel.delete');
+    });
+
+    Route::group(['prefix' => 'city'],function(){
+        Route::get('/',[CityController::class,'index'])->name('city.index');
+        Route::get('/{city}',[CityController::class,'getCity'])->name('hotel.get');
+        Route::post('/upsert',[CityController::class,'upsert'])->name('city.upsert');
+        Route::post('/delete/{city}',[CityController::class,'delete'])->name('city.delete');
     });
 });
