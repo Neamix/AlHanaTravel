24c611e7e1c58e5944a701c577806cdd8901a219
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-18 01:20:22 +0200

    edit header

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -1,8 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description"
-    content="Panagea - Premium site template for travel agencies, hotels and restaurant listing.">
+<meta name="description" content="Panagea - Premium site template for travel agencies, hotels and restaurant listing.">
 <meta name="author" content="Ansonika">
 <title>Panagea | Premium site template for travel agencies, hotels and restaurant listing.</title>
 
@@ -10,17 +9,14 @@
 <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
 <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114"
-    href="img/apple-touch-icon-114x114-precomposed.png">
-<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144"
-    href="img/apple-touch-icon-144x144-precomposed.png">
+<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
 
 <!-- GOOGLE WEB FONT -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-    rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link href="{{ asset('vendor/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
 <!-- BASE CSS -->
 <link href="{{ asset('css/front/bootstrap-rtl.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/front/style.css') }}" rel="stylesheet">
@@ -28,8 +24,7 @@
 <link href="{{ asset('css/front/vendors.css') }}" rel="stylesheet">
 
 <!-- REVOLUTION SLIDER CSS -->
-<link rel="stylesheet" type="text/css"
-    href="{{ asset('css/front/revolution-slider/fonts/font-awesome/css/font-awesome.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/front/revolution-slider/fonts/font-awesome/css/font-awesome.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/front/revolution-slider/css/settings.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/front/revolution-slider/css/layers.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/front/revolution-slider/css/navigation.css') }}">
