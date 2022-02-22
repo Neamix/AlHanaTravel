832aa50de8c85a747091204c56b07ef1999c77ce
Neamix <abdalrhmanhussin44@gmail.com>
2022-02-22 16:27:41 +0200

    localization validation auth system

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'لقد تم تغيير كلمة السر',
+    'sent' => 'لقد ارسلنا تعليمات تغير كلمة السر لك',
+    'throttled' => 'انتظر دقيقة ثم اعد المحاولة',
+    'token' => 'خطاء ما حدث',
+    'user' => "لايوجد مستخدم بهاذا البريد الاليكتروني لدينا",
+
+];
