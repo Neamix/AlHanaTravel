d795204ba9977f724c1400770be65ca92bc454c2
Neamix <abdalrhmanhussin44@gmail.com>
2022-03-04 22:40:33 +0200

    city fix

--- resources/views/components/box-small-details.blade.php
+++ resources/views/components/box-small-details.blade.php
@@ -1,5 +1,5 @@
 <div class="col-xl-3 col-lg-6 col-md-6">
-    <a href="restaurant-detail.html" class="grid_item">
+    <a href="{{ route('city.hotel',['city' => $city->id]) }}" class="grid_item">
         <figure>
             <img src="@if($city->image){{ asset('images/small/'.$city->image->name) }}@else {{ asset('img/no-image.jpg ') }} @endif" class="img-fluid" alt="">
             <div class="info">
